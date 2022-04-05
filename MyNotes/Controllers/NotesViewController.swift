29c298f48161f27549c9a62624fd643cec4b93db
Ilya <ilya.klimenyuk@gmail.com>
2022-04-05 22:40:28 +0300

    Added animation for notes list

--- MyNotes/Controllers/NotesViewController.swift
+++ MyNotes/Controllers/NotesViewController.swift
@@ -13,14 +13,12 @@ class NotesViewController: UITableViewController {
         Note(text: "The Psylogy Principles Every UI/UX Designer Needs To Know", date: Date.now),
         Note(text: "13 Things You Should Give Up If You Want To Be a Succesful UX Designer", date: Date.now),
         Note(text: "10 UI & UX Lessons from Designing My Own Product", date: Date.now),
+        Note(text: "52 Research Terms you need to know as a UX Designer", date: Date.now),
+        Note(text: "The Psylogy Principles Every UI/UX Designer Needs To Know", date: Date.now),
+        Note(text: "13 Things You Should Give Up If You Want To Be a Succesful UX Designer", date: Date.now),
+        Note(text: "10 UI & UX Lessons from Designing My Own Product", date: Date.now),
         Note(text: "52 Research Terms you need to know as a UX Designer", date: Date.now)
     ]
-    
-    // MARK: - UITableViewDelegate
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
     
     // MARK: - UITableViewDataSource
     
@@ -35,6 +33,39 @@ class NotesViewController: UITableViewController {
         cell.note = notes[indexPath.row]
         
         return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        // animation 1
+        /*
+        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -500, 10, 0)
+        cell.layer.transform = rotationTransform
+        cell.alpha = 0.5
+        
+        UIView.animate(withDuration: 1.0) {
+            cell.layer.transform = CATransform3DIdentity
+            cell.alpha = 1.0
+        }
+        */
+        
+        // animation 2
+        
+        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, 0, 50, 0)
+        cell.layer.transform = rotationTransform
+        cell.alpha = 0
+        
+        UIView.animate(withDuration: 0.75) {
+            cell.layer.transform = CATransform3DIdentity
+            cell.alpha = 1.0
+        }
+         
     }
     
 }
